6a4bc76d33b1ef79c6dde90beff79fdde318a1a9
dh-sung <dh-sung@naver.com>
2021-03-22 13:36:06 +0900

    first commit

--- build/addons/urdfreader/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dh-sung/catkin_ws/src/rbdl-rbdl-849d2aee8f4c/addons/urdfreader
-# Build directory: /home/dh-sung/catkin_ws/src/rbdl-rbdl-849d2aee8f4c/build/addons/urdfreader
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../../gtest")
---
+++ test_package/example.cpp
@@ -0,0 +1,6 @@
+// #include "rbdl_jet.h"
+#include <iostream>
+int main() {
+    // rbdl_jet();
+    std::cout<<"Sorry for no test example"<<std::endl;
+}
